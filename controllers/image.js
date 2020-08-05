fb764660631022987c13d4e706ce7ed64540ece3
Satya Sree Vedula <satyasreevedula@gmail.com>
2020-08-05 15:47:03 -0400

    Adding port

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: '07febcdb593b442c9a5d445f4e3d6bbc'
 });
 
 const handleApiCall = (req, res) => {
